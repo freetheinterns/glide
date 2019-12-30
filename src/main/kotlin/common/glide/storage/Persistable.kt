e0b9ee63b10f156425fe036df1468118bf0f9305
freetheinterns <tedtenedorio@gmail.com>
2019-12-30 01:37:33 -0800

    Fixing minor serialization bugs

--- src/main/kotlin/common/glide/storage/Persistable.kt
+++ src/main/kotlin/common/glide/storage/Persistable.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.json.Json
 import java.io.File
 
 
-abstract class Persistable<T : Persistable<T>> private constructor() {
+abstract class Persistable<T : Persistable<T>> constructor() {
   private val filename: String by lazy {
     "${this::class.simpleName}.json"
   }
@@ -29,7 +29,10 @@ abstract class Persistable<T : Persistable<T>> private constructor() {
     get() = json.stringify(serializer, thisT)
 
   fun load(): T = try {
-    json.parse(serializer, File(filename).readText())
+    json.parse(serializer, File(filename).readText()).also {
+      it.json = this.json
+      it.serializer = this.serializer
+    }
   } catch (exc: Exception) {
     println("Error loading ${this::class.simpleName} from file $filename")
     exc.printStackTrace()
